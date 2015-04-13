75c5efbedde93e70a6fc7506d6dde2327eb3ac28
莫景涛 <13677785116@163.com>
2015-04-13 17:14:35 +0800

    socket(server\client)
    test socketDemo

--- socket_client/ViewController.m
+++ socket_client/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "GCDAsyncSocket.h"
 
-@interface ViewController ()
+@interface ViewController ()<GCDAsyncSocketDelegate>
+{
+    GCDAsyncSocket *_myClient ;
+}
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -16,12 +22,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _myClient = [[GCDAsyncSocket alloc]initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    [_myClient connectToHost:@"192.168.1.104" onPort:0x8080 withTimeout:60.0 error:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)sendAction:(UIButton *)sender
+{
+    NSData *data = [_textField.text dataUsingEncoding:NSUTF8StringEncoding];
+    [_myClient writeData:data withTimeout:60 tag:10];
+}
+#pragma mark-GCDAsyncSocketDelegate
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port
+{
+    
+    //一直监听服务器端的回复
+    [sock readDataWithTimeout:-1 tag:11];
+    
+}
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    
+    //一直监听服务器端的回复
+    [sock readDataWithTimeout:-1 tag:11];
+    //收到服务器端的回复
+    _label.text =  [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding] ;
+   
+}
+- (void)socketDidCloseReadStream:(GCDAsyncSocket *)sock
+{
+    NSLog(@"关闭socket");
+}
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err
+{
+    NSLog(@"%@",[err debugDescription]);
+}
+- (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag
+{
+    NSLog(@"写入完成");
+    
+    
 
+}
 @end
